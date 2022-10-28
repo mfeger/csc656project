9e994e8433b9f916793d077a20a77913b2630dd5
mfeger <73141811+mfeger@users.noreply.github.com>
2022-10-28 12:35:06 -0700

    Add files via upload

---
+++ sum_indirect.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <random>
+#include <vector>
+#include <string.h>
+
+
+
+void 
+setup(int64_t N, uint64_t A[])
+{
+   for( int i = 0; i < N; i++ )
+      A[i] = lrand48() % N;
+   printf(" inside sum_indirect problem_setup, N=%lld \n", N);
+}
+
+int64_t
+sum(int64_t N, uint64_t A[])
+{
+   int64_t ind;
+   int64_t sum = 0;
+   for(int64_t i = 0; i < N; i++ ){
+      ind = A[i];
+      sum+=A[ind];
+   }
+
+   printf(" inside sum_indirect perform_sum, N=%lld \n", N);
+
+   return sum;
+}
+
