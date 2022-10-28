f9a99e7bdd73f61574625713299f33cdf977248f
mfeger <73141811+mfeger@users.noreply.github.com>
2022-10-28 11:29:47 -0700

    Add files via upload

---
+++ sum_harness_instructional/sum_indirect.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <random>
+#include <vector>
+#include <string.h>
+
+
+
+void 
+setup(int64_t N, uint64_t A[])
+{
+   for( int i = 0; i <= N; i++ )
+      A[i] = lrand48() % N;
+   printf(" inside sum_indirect problem_setup, N=%lld \n", N);
+}
+
+int64_t
+sum(int64_t N, uint64_t A[])
+{
+   unsigned long long sum = 0;
+   for(int64_t i = 0; i <= N; i++ )
+      sum+=A[i];
+
+      printf("sum: %d\n", sum);
+   printf(" inside sum_indirect perform_sum, N=%lld \n", N);
+
+   return 0;
+}
+
